e8c258119752ab39c259a0af29e55f2b69c7601e
SHiN201007 <shin1007m@gmail.com>
2019-09-28 16:47:35 +0900

    カレンダー日本語表示

--- カレンダー/CalendarViewController.swift
+++ カレンダー/CalendarViewController.swift
@@ -18,10 +18,23 @@ class CalendarViewController: UIViewController, FSCalendarDelegate,FSCalendarDat
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    
+    weekdays()
 
     // デリゲートの設定
     self.calendar.dataSource = self
     self.calendar.delegate = self
+  }
+  
+  // 曜日日本語化
+  func weekdays() {
+    self.calendar.calendarWeekdayView.weekdayLabels[0].text = "日"
+    self.calendar.calendarWeekdayView.weekdayLabels[1].text = "月"
+    self.calendar.calendarWeekdayView.weekdayLabels[2].text = "火"
+    self.calendar.calendarWeekdayView.weekdayLabels[3].text = "水"
+    self.calendar.calendarWeekdayView.weekdayLabels[4].text = "木"
+    self.calendar.calendarWeekdayView.weekdayLabels[5].text = "金"
+    self.calendar.calendarWeekdayView.weekdayLabels[6].text = "土"
   }
   
   fileprivate let gregorian: Calendar = Calendar(identifier: .gregorian)
